808029fc9a371fd98406093d7c037bca12978f8f
Luan <luanlam97@gmail.com>
2024-06-17 21:58:26 -0400

    test

--- src/lib/types/Template.ts
+++
@@ -1,9 +0,0 @@
-import type { Message } from "./Message";
-import type { Tool, ToolResult } from "./Tool";
-
-export type ChatTemplateInput = {
-	messages: Pick<Message, "from" | "content">[];
-	preprompt?: string;
-	tools?: Tool[];
-	toolResults?: ToolResult[];
-};
